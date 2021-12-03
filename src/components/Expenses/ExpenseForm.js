d9bd6b32925af0f94b1a14bd299ed1627d671104
unknown <you@example.com>
2021-12-03 16:36:31 +0300

    form submitions

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,25 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import './expenseForm.css'
 
-const ExpenseForm = () => {
-    const titleChangeHandler = (event) => {
-      console.log(event.target.value)
-    }
+ const ExpenseForm = () => {
+ const [newTitle, setNewTitle] = useState('');
+ const [newAmount, setNewAmount] = useState('');
+ const [newDate, setNewDate] = useState('');
+
+// const [userInput, setUserInput] = useState ({
+//     enteredTitle: '',
+//     enteredAmount: '',
+//     enteredDate: ''
+// })
+
+
+//  const dateChangeHandler = (event) => {
+//     setUserInput(() => {
+//         return ({
+//             ...userInput, 
+//             enteredDate: event.target.value
+//         })
+//     })
+//  }
+
+const titleChangeHandler = (event) => {
+   setNewTitle(event.target.value)
+}
+const amountChangeHandler = (event) => {
+    setNewAmount(event.target.value)
+}
+const dateChangeHandler = (event) => {
+    setNewDate(event.target.value)
+}
+
+const sumbitHandler = (event) => {
+  event.preventDefault();
+
+  const expenseData = {
+      title: newTitle,
+      amount: newAmount,
+      date: new Date(newDate)
+  }
+    
+ console.log(expenseData)
+}
     return (
-            <form>
+            <form onSubmit={sumbitHandler}>
                 <div className="new-expense__controls">
                   <div className="new-expense__control">
-                      <label></label>
-                      <input onChange={titleChangeHandler} type="text"/>
-                  </div>
-
-                  <div className="new-expense__control">
-                      <label>Amount</label>
+                      <label>Title:</label>
                       <input type="text"/>
                   </div>
 
                   <div className="new-expense__control">
-                      <label>Date</label>
+                      <label>Amount:</label>
+                      <input  type="text"/>
+                  </div>
+
+                  <div className="new-expense__control">
+                      <label>Date:</label>
                       <input type="date"/>
                   </div>
                 </div>
@@ -28,7 +66,7 @@ const ExpenseForm = () => {
                     <button type="submit">Add Expense</button>
                 </div>
             </form>
-    )
-}
+           )
+          }
 
 export default ExpenseForm
